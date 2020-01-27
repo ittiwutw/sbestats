729c04af65664c71c43f0a3634eb9addb10f61ef
ittiwut <ittiwut.wong@gmail.com>
2020-01-28 00:09:33 +0700

    ีupdate

--- sbEstatsApp/src/app/app.module.ts
+++ sbEstatsApp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    // Facebook,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- sbEstatsApp/src/app/login/login.page.ts
+++ sbEstatsApp/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,22 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   Login() {
+    // this.fb.login(['public_profile', 'user_friends', 'email'])
+    //   .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
+    //   .catch(e => console.log('Error logging into Facebook', e));
+
+
+    // this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
     // this.storage.set('currentUser', 'test');
-    this.router.navigateByUrl('/tabs');
+    // this.router.navigateByUrl('/tabs');
   }
 
 }
