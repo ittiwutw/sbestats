ad9425a94770be771ed3bb879a600818d54799e1
ittiwut <ittiwut.wong@gmail.com>
2020-05-06 03:34:30 +0700

    upload file

--- sbEstateNodeService/service/estate-service.js
+++ sbEstateNodeService/service/estate-service.js
@@ -1,5 +1,7 @@
 var _estateRepo = require("../repository/estate-repository");
 var _coreFunc = require("./coreFunction");
+var fs = require("fs");
+var host = "https://sbestate.yuzudigital.com/";
 
 async function getEstate(param, callback) {
   var data = await _estateRepo.getEstate(param);
@@ -20,6 +22,41 @@ async function getEstateImg(estateId) {
 }
 
 async function saveEstate(param, callback) {
+  if (!fs.existsSync("estate")) {
+    fs.mkdirSync("estate");
+  }
+  if (param.imgUrl.length > 0) {
+    var base64Data = param.imgUrl.replace(/^data:image\/png;base64,/, "");
+    fs.writeFile("estate/" + makeid(5) + ".png", base64Data, "base64", function (
+      err
+    ) {});
+    param.imgUrl = host + "estate/" + makeid(5) + ".png";
+  }
+
+  if (param.deedImg.length > 0) {
+    var base64Data = param.deedImg.replace(/^data:image\/png;base64,/, "");
+    fs.writeFile("estate/" + makeid(5) + ".png", base64Data, "base64", function (
+      err
+    ) {});
+    param.deedImg = host + "estate/" + makeid(5) + ".png";
+  }
+
+  if (param.landImg.length > 0) {
+    var base64Data = param.landImg.replace(/^data:image\/png;base64,/, "");
+    fs.writeFile("estate/" + makeid(5) + ".png", base64Data, "base64", function (
+      err
+    ) {});
+    param.landImg = host + "estate/" + makeid(5) + ".png";
+  }
+
+  if (param.positionImg.length > 0) {
+    var base64Data = param.positionImg.replace(/^data:image\/png;base64,/, "");
+    fs.writeFile("estate/" + makeid(5) + ".png", base64Data, "base64", function (
+      err
+    ) {});
+    param.positionImg = host + "estate/" + makeid(5) + ".png";
+  }
+
   param.createDate = new Date().toMysqlFormat();
   var data = await _estateRepo.saveEstate(param);
 
@@ -29,6 +66,17 @@ async function saveEstate(param, callback) {
   });
 
   return;
+}
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 async function updateEstate(param, callback) {
